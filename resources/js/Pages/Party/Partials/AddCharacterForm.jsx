4dec216e33d9fd247e7206c7d252722ba6b0df19
capashilli <capashilli@gmail.com>
2023-02-12 03:12:00 -0800

    Add stub React components for the added stuff, add route

---
+++ resources/js/Pages/Party/Partials/AddCharacterForm.jsx
@@ -0,0 +1,22 @@
+import { useRef, useState } from 'react';
+import DangerButton from '@/Components/DangerButton';
+import InputError from '@/Components/InputError';
+import InputLabel from '@/Components/InputLabel';
+import Modal from '@/Components/Modal';
+import SecondaryButton from '@/Components/SecondaryButton';
+import TextInput from '@/Components/TextInput';
+import { useForm } from '@inertiajs/react';
+
+export default function AddCharacterForm({ className }) {
+    const [confirmingUserDeletion, setConfirmingUserDeletion] = useState(false);
+
+    const addCharacter = () => {
+        
+    }
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
