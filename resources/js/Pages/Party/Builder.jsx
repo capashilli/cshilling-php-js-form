a894284828643ec925dc4381f3d169f32b6173d6
capashilli <capashilli@gmail.com>
2023-02-13 02:25:29 -0800

    Styling, navigation, basic copy

--- resources/js/Pages/Party/Builder.jsx
+++ resources/js/Pages/Party/Builder.jsx
@@ -1,4 +1,4 @@
-import { Head } from '@inertiajs/react';
+import { Link, Head } from '@inertiajs/react';
 import AddCharacterForm from './Partials/AddCharacterForm';
 import PartyList from './Partials/PartyList';
 
@@ -6,7 +6,7 @@ export default function Builder({ ancestries, backgrounds, characters, character
     
     return (
         <div className="bg-gray-100 dark:bg-gray-900 w-full h-full">
-            <div className="bg-parchment max-w-3xl min-h-screen mx-auto shadow-lg px-12 pt-20">
+            <div className="bg-parchment max-w-3xl min-h-screen mx-auto shadow-lg px-12 pt-20 flex flex-col content-center">
                 <Head title="Party Builder" />
                 <PartyList
                     characters={characters}    
@@ -16,6 +16,7 @@ export default function Builder({ ancestries, backgrounds, characters, character
                     backgrounds={backgrounds}
                     characterClasses={characterClasses}
                 ></AddCharacterForm>
+                <Link className="text-center mt-4 hover:text-cyan-700 transition-colors" href="/">Take me back</Link>
             </div>
         </div>
     );
